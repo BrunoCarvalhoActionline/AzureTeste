4c0b8708ef0d52b52fc6fbd3d6ba4c522e6b9ad1
Bruno Cursi <bruno.carvalho@actionline.com.br>
2019-05-09 12:12:14 -0300

    Alteração no value para bruno

--- AzureTeste/Controllers/ValuesController.cs
+++ AzureTeste/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace AzureTeste.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Bruno", "value2" };
         }
 
         // GET api/values/5
